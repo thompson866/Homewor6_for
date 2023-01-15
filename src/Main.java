acb13b6ccdacc30e37923ac7b964ec2d62d78e92
arxip <arxipov1989@gmail.com>
2023-01-15 18:20:21 +0300

    Домашка цикл v1

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,22 @@ public class Main {
 
         //Задача 1//
         System.out.println("Task 1");
-        for ( int q = 1; q <= 10; q ++) {
-        System.out.println(q);}
+        for (int q = 1; q <= 10; q++) {
+            System.out.println(q);
+        }
         //Задача 2//
         System.out.println("Task 2");
-        for (int q = 10; q > 0; q = q -1) {
-        System.out.println(q);
+        for (int q = 10; q > 0; q--) {
+            System.out.println(q);
         }
         //Задача 3//
         System.out.println("Task 3");
-        for (int q = 2; q <=17; q = q + 2) {
+        for (int q = 2; q <= 17; q = q + 2) {
             System.out.println(q);
         }
         //Задача 4//
         System.out.println("Task 4");
-        for (int q = 10; q >= -10; q = q - 1) {
+        for (int q = 10; q >= -10; q--) {
             System.out.println(q);
         }
         //Задача 5//
@@ -27,40 +28,39 @@ public class Main {
         }
         //Задача 6//
         System.out.println("Task 6");
-        for (int q = 7; q < 100; q = q + 7){
+        for (int q = 7; q < 100; q = q + 7) {
             System.out.println(q);
         }
         //Задача 7//
         System.out.println("Task 7");
-        for (int q = 1; q < 600; q = q * 2){
+        for (int q = 1; q < 600; q = q * 2) {
             System.out.println(q);
         }
         System.out.println("Task 8");
         //Задача 8//
         int money = 29000;
         int moneyTotal = 0;
-        for (int i = 1; i<=12; i++){
+        int months = 12;
+        for (int month = 1; month <= months; month++) {
             moneyTotal = moneyTotal + money;
-            System.out.println("Месяц " + i +" сумма накоплений равна " + moneyTotal + " рублей");
+            System.out.println("Месяц " + month + " сумма накоплений равна " + moneyTotal + " рублей");
         }
 
         //Задача 9//
         System.out.println("Task 9");
-        int salary = 29000;
-        int total = 0;
-        for (int i = 1; i<=12; i++) {
-            total = total + total/100*12;
-            total = total + money;
-            System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей");
+        double totalPercent = 0;
+        double percent = 0.01;
+        for (int month = 1; month <= months; month++) {
+            totalPercent = totalPercent + money + (totalPercent * percent);
+
+            System.out.println("Месяц " + month + " сумма накоплений равна " + totalPercent + " рублей");
         }
 
         //Задача 10//
         System.out.println("Task 10");
         int number = 2;
-        for (int i=1; i<11; i++){
-            System.out.println(number * i);
-
+        for (int num = 1; num < 11; num++) {
+            System.out.printf("%d * %d = %d%n", number, num, number * num);
         }
-
     }
 }
